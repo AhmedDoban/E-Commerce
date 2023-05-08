4281983fee733d7cd90321f5061eec4cd0e135d8
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2023-05-08 13:36:19 +0300

    github pages

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
         }
       >
         <Routes>
+          <Route path="/home" element={<Home />} />
           <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFounded />} />
         </Routes>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
-import "./css/normalize.css";
 import "./css/mian.css";
-import "./css/root.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

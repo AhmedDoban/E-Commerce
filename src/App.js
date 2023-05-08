b003c011a1b08efb220dda9aeb244d3246fe979c
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2023-05-08 13:59:53 +0300

    back to the top

--- src/App.js
+++ src/App.js
@@ -9,25 +9,27 @@ const NotFounded = lazy(() => import("./Js/Not Founded/NotFounded"));
 function App() {
   return (
     <div className="App">
-      <Suspense
-        fallback={
-          <div className="pre-loader">
-            <Player
-              autoplay={true}
-              loop={true}
-              controls={false}
-              src="https://assets10.lottiefiles.com/packages/lf20_p8bfn5to.json"
-              className="palyer"
-            />
-          </div>
-        }
-      >
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFounded />} />
-        </Routes>
-      </Suspense>
+      <div className="page-wrpper">
+        <Suspense
+          fallback={
+            <div className="pre-loader">
+              <Player
+                autoplay={true}
+                loop={true}
+                controls={false}
+                src="https://assets10.lottiefiles.com/packages/lf20_p8bfn5to.json"
+                className="palyer"
+              />
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFounded />} />
+          </Routes>
+        </Suspense>
+      </div>
     </div>
   );
 }

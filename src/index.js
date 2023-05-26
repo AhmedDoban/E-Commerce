f562c60ef4eefa2a01b29d17128cf05500cb4dae
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2023-05-26 20:58:51 +0300

    update style

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
-import "./assets/css/mian.css";
+import "./assets/css/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

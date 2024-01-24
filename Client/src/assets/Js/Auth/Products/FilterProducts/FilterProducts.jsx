ebbe1edb920b015cc9b86dbdd69e65bb5cf527e7
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2024-01-24 15:38:10 +0200

    update respose

--- Client/src/assets/Js/Auth/Products/FilterProducts/FilterProducts.jsx
+++ Client/src/assets/Js/Auth/Products/FilterProducts/FilterProducts.jsx
@@ -9,6 +9,7 @@ function FilterProducts() {
   const Search = useSelector((state) => state.Products.Search);
   const Dispatch = useDispatch();
   const [SearchInput, SetSearchInput] = useState(Search);
+  const [ShowFilter, SetShowFilter] = useState(true);
 
   const [Value, SetValue] = useState({
     CategoryFilter: "",
@@ -92,63 +93,85 @@ function FilterProducts() {
   return (
     <React.Fragment>
       <div className="FilterProducts">
-        <div className="Filter">
-          <Select
-            options={CategoryOptions}
-            onChange={(OptionValue) =>
-              SetValue({
-                ...Value,
-                CategoryFilter:
-                  OptionValue[0] === undefined ? "" : OptionValue[0].value,
-              })
-            }
-            placeholder="Select an Category"
-            multi={false}
-            dropdownPosition="auto"
-            separator={false}
-            className="SelectELEMENT"
-            clearable={true}
-          />
-          <Select
-            options={StarRate}
-            onChange={(OptionValue) =>
-              SetValue({ ...Value, StarRate: OptionValue[0].value })
-            }
-            placeholder="Select an Star Rate"
-            dropdownPosition="auto"
-            separator={false}
-            className="SelectELEMENT"
-            searchBy="value"
-          />
-          <div className="input-box">
-            <input
-              type="number"
-              placeholder="MIN"
-              value={Value.Min}
-              onChange={(e) => SetValue({ ...Value, Min: e.target.value })}
-              min={0}
-            />
+        {ShowFilter && (
+          <div className="Filter">
+            <div className="select-box">
+              <Select
+                options={CategoryOptions}
+                onChange={(OptionValue) =>
+                  SetValue({
+                    ...Value,
+                    CategoryFilter:
+                      OptionValue[0] === undefined ? "" : OptionValue[0].value,
+                  })
+                }
+                placeholder="Select an Category"
+                multi={false}
+                dropdownPosition="auto"
+                separator={false}
+                className="SelectELEMENT"
+                clearable={true}
+              />
+              <Select
+                options={StarRate}
+                onChange={(OptionValue) =>
+                  SetValue({
+                    ...Value,
+                    StarRate:
+                      OptionValue[0] === undefined ? "" : OptionValue[0].value,
+                  })
+                }
+                placeholder="Select an Star Rate"
+                dropdownPosition="auto"
+                separator={false}
+                className="SelectELEMENT"
+                searchBy="value"
+                clearable={true}
+              />
+            </div>
+            <div className="input-action">
+              <div className="input-box">
+                <input
+                  type="number"
+                  placeholder="MIN"
+                  value={Value.Min}
+                  onChange={(e) => SetValue({ ...Value, Min: e.target.value })}
+                  min={0}
+                />
+              </div>
+              <div className="input-box">
+                <input
+                  type="number"
+                  placeholder="MAX"
+                  value={Value.Max}
+                  onChange={(e) => SetValue({ ...Value, Max: e.target.value })}
+                  min={1}
+                />
+              </div>
+            </div>
           </div>
-          <div className="input-box">
-            <input
-              type="number"
-              placeholder="MAX"
-              value={Value.Max}
-              onChange={(e) => SetValue({ ...Value, Max: e.target.value })}
-              min={1}
-            />
-          </div>
-        </div>
+        )}
+
         <div className="action">
-          <input
-            type="search"
-            name="Search"
-            id="Search"
-            value={SearchInput}
-            placeholder="Search Product"
-            onChange={(e) => SetSearchInput(e.target.value)}
-          />
-          <button>Search</button>
+          <i
+            className={
+              ShowFilter
+                ? "fa-solid fa-filter-circle-xmark"
+                : "fa-solid fa-filter active"
+            }
+            onClick={() => SetShowFilter((prev) => !prev)}
+          ></i>
+          <div className="Search-box">
+            <input
+              type="search"
+              name="Search"
+              id="Search"
+              value={SearchInput}
+              placeholder="Search Product"
+              onChange={(e) => SetSearchInput(e.target.value)}
+            />
+            <button>Search</button>
+          </div>
         </div>
       </div>
     </React.Fragment>
